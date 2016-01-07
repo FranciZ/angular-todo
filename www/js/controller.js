45e83d150adfb6604dc1cb62b44246fd6fffe83d
Franci Zidar <franci@proxima.si>
2016-01-07 21:23:41 +0100

    first commit

---
+++ www/js/controller.js
@@ -0,0 +1,92 @@
+angular.module('starter').controller('TodoCtrl', function($scope, $localForage){
+   
+    $scope.model = {};
+    $scope.items = [];
+    
+      $localForage.getItem('myItems').then(function(data) {
+
+          $scope.items = data;
+          getNumLeft();
+
+        });
+    
+    $scope.$watch('items', function(newVal, oldVal){
+    
+        updateItems();
+        
+    }, true);
+    
+    function updateItems(){
+        
+        $localForage.setItem('myItems', $scope.items).then(function(){
+               
+        });
+        
+    }
+    
+    $scope.toggleDone = function(item){
+      
+        item.done = !item.done;
+        getNumLeft();
+        
+    };
+    
+    $scope.onEnter = function(item){
+        
+        item.over = true;
+        
+    };
+    
+    $scope.removeItem = function(index){
+      
+        $scope.items.splice(index, 1);
+        
+        getNumLeft();
+        
+    };
+    
+    $scope.onLeave = function(item){
+      
+        item.over = false;
+        
+    };
+    
+    function getNumLeft(){
+        
+        var numLeft = 0;
+        
+        for(var i=0;i<$scope.items.length;i++){
+            
+            var item = $scope.items[i];
+            
+            if(!item.done){
+                
+                numLeft++;
+                
+            }
+            
+        }
+        
+        $scope.model.numLeft = numLeft;
+        
+    }
+    
+    $scope.onKeyPress = function(evt){
+        
+        if(evt.keyCode === 13){
+            
+            // enter/return pressed
+            
+            $scope.items.push({ title:$scope.model.newTodo });
+            
+            $scope.model.newTodo = '';
+            
+            getNumLeft();
+               
+        }
+        
+    };
+    
+    getNumLeft();
+    
+});
